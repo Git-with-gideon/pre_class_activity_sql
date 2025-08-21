ac375522c2b47200f5abb073fa25cb6eb7dd8277
meghan-ke <m.mwizerwa@alustudent.com>
2025-08-21 20:22:39 +0200

    linux grades

--- student_perfomance.py
+++ student_perfomance.py
@@ -4,3 +4,11 @@ CREATE TABLE IF NOT EXISTS students (
     student_name VARCHAR(255) NOT NULL,
     intake_year YEAR NOT NULL
 );
+# table for the linux grades
+CREATE TABLE IF NOT EXISTS linux_grades (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(255) NOT NULL,
+    student_id INT,
+    grade_obtained INT NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
